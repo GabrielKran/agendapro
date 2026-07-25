b6981d379bd3aa98097061e438252a7dd11c403f
GabrielKran <gabrielkran@gmail.com>
2026-07-25 18:36:16 -0300

    feat: remove SpringBootTest annotation from tests

--- core/src/test/java/com/agendapro/core/CoreApplicationTests.java
+++ core/src/test/java/com/agendapro/core/CoreApplicationTests.java
@@ -1,9 +1,7 @@
 package com.agendapro.core;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CoreApplicationTests {
 
 	@Test
